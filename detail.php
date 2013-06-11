cc7604cfd4bb7a072575edb2717bc3c3b699e2fa
manu <ma.lindofer@gmail.com>
2013-06-11 13:31:06 +0200

    asdf

--- detail.php
+++ detail.php
@@ -107,7 +107,7 @@
 		</div>-->
 		
 		<div class="ui-grid-solo">
-			<div class="DivParent" style="height: 150px;padding-right: 10%;">
+			<div class="DivParent" style="height: 150px;padding-right: 10%;margin-top: 3%;">
 					<div class="DivWhichNeedToBeVerticallyAligned" style="width: 50%;">
 						<div class="ui-block-a" style="margin-right: 3%;text-align: right;">
 							<canvas id="detail_canvas" height="150" width="150"></canvas>
